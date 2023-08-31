61bb8f237ac7bab460fca87227665b22d2aa170e
Julian Ruiz Ocampo <124713530+JuliRuizOcampo@users.noreply.github.com>
2023-08-31 04:47:19 -0500

    hhh

---
+++ src/main/java/com/mycompany/prueba3/Prueba3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba3;
+
+/**
+ *
+ * @author julir
+ */
+public class Prueba3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
